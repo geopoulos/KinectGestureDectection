5f4920700d99fb247bea6b65eadf5a9e090c2995
George Poulos <poulos@gmail.com>
2011-11-17 18:46:34 -0500

    Gesture detection skeleton with a simple swipe gesture detector.

---
+++ KinectGestureDectection/MainWindow.xaml.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using Microsoft.Research.Kinect.Nui;
+using Kinect.Toolbox;
+
+namespace KinectGestureDectection
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Runtime kinectRuntime;
+        private SkeletonDisplayManager skeletonDisplayManager;
+
+        private readonly ColorStreamManager streamManager = new ColorStreamManager();
+        private readonly GestureDetector gestureRecognizer = new SimpleSlashGestureDetector();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Initialize()
+        {
+            if (kinectRuntime == null)
+                return;
+
+            kinectRuntime.Initialize(RuntimeOptions.UseSkeletalTracking | RuntimeOptions.UseColor);
+            kinectRuntime.VideoStream.Open(ImageStreamType.Video, 2, ImageResolution.Resolution640x480, ImageType.Color);
+            kinectRuntime.VideoFrameReady += new EventHandler<ImageFrameReadyEventArgs>(kinectRuntime_VideoFrameReady);
+            kinectRuntime.SkeletonFrameReady += new EventHandler<SkeletonFrameReadyEventArgs>(kinectRuntime_SkeletonFrameReady);
+            skeletonDisplayManager = new SkeletonDisplayManager(kinectRuntime.SkeletonEngine, kinectCanvas);
+            
+            gestureRecognizer.TraceTo(gesturesCanvas, System.Windows.Media.Color.FromRgb(255, 0, 0));
+            gestureRecognizer.OnGestureDetected += new Action<string>(OnGestureDetected);
+            gestureRecognizer.MinimalPeriodBetweenGestures = 2000;
+        }
+
+        private void OnGestureDetected(string gesture)
+        {
+            System.Diagnostics.Debug.WriteLine(DateTime.Now.Ticks + " " + gesture);
+        }
+
+        private void Clean()
+        {
+            gestureRecognizer.OnGestureDetected -= OnGestureDetected;
+
+            if (kinectRuntime != null)
+            {
+                kinectRuntime.VideoFrameReady -= kinectRuntime_VideoFrameReady;
+                kinectRuntime.SkeletonFrameReady -= kinectRuntime_SkeletonFrameReady;
+                kinectRuntime.Uninitialize();
+                kinectRuntime = null;
+            }
+        }
+
+        private void ProcessFrame(SkeletonFrame frame)
+        {
+            foreach (var skeleton in frame.Skeletons)
+            {
+                if (skeleton.TrackingState != SkeletonTrackingState.Tracked)
+                    continue;
+
+                foreach (Joint joint in skeleton.Joints)
+                {
+                    if (joint.Position.W < 0.8f || joint.TrackingState != JointTrackingState.Tracked)
+                        continue;
+
+                    if (joint.ID == JointID.HandRight)
+                    {
+                        gestureRecognizer.Add(joint.Position, kinectRuntime.SkeletonEngine);
+                    }
+                }
+
+                skeletonDisplayManager.Draw(frame);
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                foreach (Runtime kinect in Runtime.Kinects)
+                {
+                    if (kinect.Status == KinectStatus.Connected)
+                    {
+                        kinectRuntime = kinect;
+                        break;
+                    }
+                }
+
+                if (Runtime.Kinects.Count == 0)
+                    MessageBox.Show("No Kinect Found");
+                else
+                    Initialize();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            Clean();
+        }
+
+        private void kinectRuntime_VideoFrameReady(object sender, ImageFrameReadyEventArgs e)
+        {
+            kinectDisplay.Source = streamManager.Update(e);
+        }
+
+        private void kinectRuntime_SkeletonFrameReady(object sender, SkeletonFrameReadyEventArgs e)
+        {
+            if (e.SkeletonFrame == null)
+                return;
+            if (!e.SkeletonFrame.Skeletons.Any(s => s.TrackingState != SkeletonTrackingState.NotTracked))
+                return;
+            ProcessFrame(e.SkeletonFrame);
+        }
+
+    }
+}
---
+++ KinectGestureDectection/SimpleSwipeGestureDetector.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Research.Kinect.Nui;
+using Kinect.Toolbox;
+
+namespace KinectGestureDectection
+{
+    class SimpleSlashGestureDetector : GestureDetector
+    {
+        protected const float SwipeMinimalLength = 0.3f;
+        protected const int SwipeMinimalDuration = 250;
+        protected const int SwipeMaximalDuration = 1000;
+
+        protected bool ScanPositions(Func<Vector3, Vector3, bool> directionFunction, Func<Vector3, Vector3, bool> lengthFunction, int minTime, int maxTime)
+        {
+            int start = 0;
+
+            for (int index = 1; index < Entries.Count - 1; index++)
+            {
+                if (!directionFunction(Entries[index].Position, Entries[index + 1].Position))
+                {
+                    start = index;
+                }
+
+                if (lengthFunction(Entries[index].Position, Entries[start].Position))
+                {
+                    double totalMilliseconds = (Entries[index].Time - Entries[start].Time).TotalMilliseconds;
+                    if (totalMilliseconds >= minTime && totalMilliseconds <= maxTime)
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        protected override void LookForGesture()
+        {
+            if (ScanPositions(
+                    (p1, p2) => p2.Y - p1.Y < 0.01f,
+                    (p1, p2) => Math.Abs(p2.Y - p1.Y) > SwipeMinimalLength,
+                    SwipeMinimalDuration, SwipeMaximalDuration))
+            {
+                RaiseGestureDetected("SlashUpToDown");
+                return;
+            }
+
+            if (ScanPositions(
+                    (p1, p2) => p2.Y - p1.Y > -0.01f,
+                    (p1, p2) => Math.Abs(p2.Y - p1.Y) > SwipeMinimalLength,
+                    SwipeMinimalDuration, SwipeMaximalDuration))
+            {
+                RaiseGestureDetected("SlashDownToUp");
+                return;
+            }
+
+            if (ScanPositions(
+                    (p1, p2) => p2.X - p1.X > -0.01f,
+                    (p1, p2) => Math.Abs(p2.X - p1.X) > SwipeMinimalLength,
+                    SwipeMinimalDuration, SwipeMaximalDuration))
+            {
+                RaiseGestureDetected("SlashLeftToRight");
+                return;
+            }
+
+            if (ScanPositions(
+                    (p1, p2) => p2.X - p1.X < 0.01f,
+                    (p1, p2) => Math.Abs(p2.X - p1.X) > SwipeMinimalLength,
+                    SwipeMinimalDuration, SwipeMaximalDuration))
+            {
+                RaiseGestureDetected("SlashRightToLeft");
+                return;
+            }
+
+        }
+    }
+}
